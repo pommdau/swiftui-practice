f1af5004ec74f7a49f4b0a1eb0b43dd2aac99212
IKEH1024 <pondau1024@gmail.com>
2022-02-27 13:54:03 +0900

    keep reading

---
+++ Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by HIROKI IKEUCHI on 2022/02/27.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
