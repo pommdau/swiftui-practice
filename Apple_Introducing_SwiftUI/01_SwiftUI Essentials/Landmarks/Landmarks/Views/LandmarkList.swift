cfed16309ee5f5adb301df5a6c315dad906bd25d
IKEH1024 <pondau1024@gmail.com>
2022-02-27 17:02:33 +0900

    finish reading "Building Lists and Navigation"

--- Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkList.swift
+++ Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -24,6 +24,12 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+        
     }
 }
