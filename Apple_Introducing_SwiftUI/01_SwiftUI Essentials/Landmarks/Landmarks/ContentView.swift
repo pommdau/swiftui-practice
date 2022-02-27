b8546506077b84f215fc3f4bed4256503628fc70
IKEH1024 <pondau1024@gmail.com>
2022-02-27 14:08:32 +0900

    finish reading "SwiftUI Essentials Creating and Combining Views"

--- Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/ContentView.swift
+++ Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/ContentView.swift
@@ -9,18 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Text("Hello, SwiftUI!!")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
