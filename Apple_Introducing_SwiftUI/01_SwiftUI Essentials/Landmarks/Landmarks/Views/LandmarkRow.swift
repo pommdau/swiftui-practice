bff01163fd716248e9b6305687dba0fd4cd5ca5b
IKEH1024 <pondau1024@gmail.com>
2022-02-27 16:33:12 +0900

    keep reading

--- Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkRow.swift
+++ Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -25,6 +25,10 @@ struct LandmarkRow: View {
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarks[0])
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
