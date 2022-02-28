caee1421b18d698780b5d43c46477cd2df3c1550
IKEH1024 <pondau1024@gmail.com>
2022-02-28 21:52:58 +0900

    keep reading

--- Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkList.swift
+++ Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            // showFavoritesOnlyがfalseのときフィルタリングはされない
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
@@ -25,11 +34,13 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+//        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+//            LandmarkList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
+        
+        LandmarkList()
         
     }
 }
--- Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkRow.swift
+++ Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -19,6 +19,11 @@ struct LandmarkRow: View {
             Text(landmark.name)
 
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
