5fe26a4083859d39e3cc0cdfc75518ff3b6daad0
IKEH1024 <pondau1024@gmail.com>
2022-02-28 21:56:56 +0900

    keep reading

--- Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkList.swift
+++ Apple_Introducing_SwiftUI/01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -19,12 +19,19 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
                 }                
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
             }
             .navigationTitle("Landmarks")
         }
