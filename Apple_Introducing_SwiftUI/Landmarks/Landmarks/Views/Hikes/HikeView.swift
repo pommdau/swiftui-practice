ce63665dc0e12175230e0f723f8db5c63567a479
IKEH1024 <pondau1024@gmail.com>
2022-03-01 11:31:28 +0900

    keep reading

--- Apple_Introducing_SwiftUI/Landmarks/Landmarks/Views/Hikes/HikeView.swift
+++ Apple_Introducing_SwiftUI/Landmarks/Landmarks/Views/Hikes/HikeView.swift
@@ -1,30 +1,39 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view displaying information about a hike, including an elevation graph.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view displaying information about a hike, including an elevation graph.
+ */
 
 import SwiftUI
+
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        .asymmetric(
+            insertion: .move(edge: .trailing).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity)
+        )
+    }
+}
 
 struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
-
+    
     var body: some View {
         VStack {
             HStack {
                 HikeGraph(hike: hike, path: \.elevation)
                     .frame(width: 50, height: 30)
-
+                
                 VStack(alignment: .leading) {
                     Text(hike.name)
                         .font(.headline)
                     Text(hike.distanceText)
                 }
-
+                
                 Spacer()
-
+                
                 Button {
                     withAnimation {
                         showDetail.toggle()
@@ -38,9 +47,10 @@ struct HikeView: View {
                         .padding()
                 }
             }
-
+            
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade)
             }
         }
     }
