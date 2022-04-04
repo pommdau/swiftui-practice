9a23a77f766b7ce684a1344ba60096eef90d67b6
IKEH1024 <pondau1024@gmail.com>
2022-04-04 14:12:53 +0900

    keep reading

--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/WelcomeView.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/WelcomeView.swift
@@ -3,29 +3,8 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         ZStack {
-            WelcomeBackgroundImage()      
-            Label {
-                VStack(alignment: .leading) {
-                    Text("Welcome to")
-                        .font(.headline)
-                        .bold()
-                    Text("Kuchi")
-                        .font(.largeTitle)
-                        .bold()
-                }
-                .foregroundColor(.red)
-                .lineLimit(1)
-                .padding(.horizontal)
-            } icon: {
-                Image(systemName: "table")
-                    .resizable()
-                    .frame(width: 30, height: 30)
-                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-                    .background(Color(white: 0.9))
-                    .clipShape(Circle())
-                    .foregroundColor(.red)
-            }
-            .labelStyle(HorizontallyAlignedLabelStyle())
+            WelcomeBackgroundImage()
+            WelcomeMessageView()
         }
     }
 }
