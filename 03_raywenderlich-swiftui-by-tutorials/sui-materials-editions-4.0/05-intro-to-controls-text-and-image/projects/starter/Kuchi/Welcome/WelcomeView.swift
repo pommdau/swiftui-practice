d2253eb13c1d6e90db333c7c11a89364aa44e4bd
IKEH1024 <pondau1024@gmail.com>
2022-04-04 11:51:34 +0900

    keep reading

--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/05-intro-to-controls-text-and-image/projects/starter/Kuchi/Welcome/WelcomeView.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/05-intro-to-controls-text-and-image/projects/starter/Kuchi/Welcome/WelcomeView.swift
@@ -12,7 +12,20 @@ struct WelcomeView: View {
                 .blur(radius: 5)
                 .opacity(0.08)
             
-            HStack {
+            Label {
+                VStack(alignment: .leading) {
+                    Text("Welcome to")
+                        .font(.headline)
+                        .bold()
+                    Text("Kuchi")
+                        .font(.largeTitle)
+                        .bold()
+                }
+                .foregroundColor(.red)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+                .padding(.horizontal)                
+            } icon: {
                 Image(systemName: "table")
                     .resizable()
                     .frame(width: 30, height: 30)
@@ -20,17 +33,9 @@ struct WelcomeView: View {
                     .background(Color(white: 0.9))
                     .clipShape(Circle())
                     .foregroundColor(.red)
-                
-                VStack(alignment: .leading) {
-                    Text("**Welcome to**")
-                        .font(.headline)
-                    Text("**Kuchi**")
-                        .font(.largeTitle)
-                }
-                .foregroundColor(.red)
-                .lineLimit(1)
-                .padding(.horizontal)
             }
+            .labelStyle(HorizontallyAlignedLabelStyle())
+            
         }
     }
 }
