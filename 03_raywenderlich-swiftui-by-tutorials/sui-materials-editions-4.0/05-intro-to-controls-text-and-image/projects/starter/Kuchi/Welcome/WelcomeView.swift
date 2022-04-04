8b020c6a657f9e8c0ff308d7ba69cfbb7ab4541c
IKEH1024 <pondau1024@gmail.com>
2022-04-04 11:38:08 +0900

    keep reading

--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/05-intro-to-controls-text-and-image/projects/starter/Kuchi/Welcome/WelcomeView.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/05-intro-to-controls-text-and-image/projects/starter/Kuchi/Welcome/WelcomeView.swift
@@ -5,7 +5,7 @@ struct WelcomeView: View {
         ZStack {
             Image("welcome-background", bundle: nil)
                 .resizable()
-//                .scaledToFit()
+            //                .scaledToFit()
                 .aspectRatio(1 / 1, contentMode: .fill)
                 .edgesIgnoringSafeArea(.all)
                 .saturation(0.5)
@@ -14,19 +14,24 @@ struct WelcomeView: View {
             
             HStack {
                 Image(systemName: "table")
-                  .resizable()
-                  .frame(width: 30, height: 30)
-                  .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-                  .background(Color(white: 0.9))
-                  .clipShape(Circle())
-                  .foregroundColor(.red)
-
-                Text("Welcome to Kuchi")
-                  .font(.system(size: 30))
-                  .bold()
-                  .foregroundColor(.red)
-                  .lineLimit(2)
-                  .multilineTextAlignment(.center)
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                    .background(Color(white: 0.9))
+                    .clipShape(Circle())
+                    .foregroundColor(.red)
+                
+                VStack(alignment: .leading) {
+                    Text("Welcome to")
+                        .font(.headline)
+                        .bold()
+                    Text("Kuchi")
+                        .font(.largeTitle)
+                        .bold()
+                }
+                .foregroundColor(.red)
+                .lineLimit(1)
+                .padding(.horizontal)
             }
         }
     }
