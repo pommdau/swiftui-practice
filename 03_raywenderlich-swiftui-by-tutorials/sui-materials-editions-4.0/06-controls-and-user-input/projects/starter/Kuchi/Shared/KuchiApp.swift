3f016b7607e210ea668484b3d5c90909904e4998
IKEH1024 <pondau1024@gmail.com>
2022-04-04 20:43:48 +0900

    keep reading

--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/06-controls-and-user-input/projects/starter/Kuchi/Shared/KuchiApp.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/06-controls-and-user-input/projects/starter/Kuchi/Shared/KuchiApp.swift
@@ -2,15 +2,27 @@ import SwiftUI
 
 @main
 struct KuchiApp: App {
+    
+    let userManager = UserManager()
+    
+    init() {
+        userManager.load()
+    }
+    
     var body: some Scene {
         WindowGroup {
             RegisterView()
+                .environmentObject(userManager)
         }
     }
 }
 
 struct KuchiApp_Previews: PreviewProvider {
+    
+    static let userManager = UserManager(name: "Ray")
+    
     static var previews: some View {
         RegisterView()
+            .environmentObject(userManager)
     }
 }
--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
@@ -2,14 +2,19 @@ import SwiftUI
 
 struct RegisterView: View {
     
-    @State var name: String = ""
+    @FocusState var nameFieldFocused: Bool
+    @EnvironmentObject var userManager: UserManager
     
     var body: some View {
         VStack {
             Spacer()
             WelcomeMessageView()
-            TextField("Type your name...", text: $name)
+            TextField("Type your name...", text: $userManager.profile.name)
+                .submitLabel(.done)
                 .bordered()
+            Button(action: registerUser) {
+                Text("OK")
+            }
             Spacer()
         }
         .padding()
@@ -17,8 +22,19 @@ struct RegisterView: View {
     }
 }
 
+// MARK: - Event Handlers
+extension RegisterView {
+    func registerUser() {
+        userManager.persistProfile()
+    }
+}
+
 struct RegisterView_Previews: PreviewProvider {
+    
+    static let user = UserManager(name: "Ray")
+    
     static var previews: some View {
         RegisterView()
+            .environmentObject(user)
     }
 }
