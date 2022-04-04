2658455343f0ae7816c59f4bb99911ebc21fac33
IKEH1024 <pondau1024@gmail.com>
2022-04-04 09:31:27 +0900

    keep reading

--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/04-testing-and-debugging/projects/starter/SwiftCalc/MemoryView.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/04-testing-and-debugging/projects/starter/SwiftCalc/MemoryView.swift
@@ -5,14 +5,21 @@ struct MemoryView: View {
     var geometry: GeometryProxy
     
     var body: some View {
-        
+#if targetEnvironment(macCatalyst)
+        let doubleTap = TapGesture(count: 2)
+            .onEnded { _ in
+                self.memory = 0.0
+            }
+#else
         let memorySwipe = DragGesture(minimumDistance: 20)
             .onEnded { _ in
-                memory = 0.0
+                self.memory = 0.0
             }
+#endif
         
         HStack {
             Spacer()
+#if targetEnvironment(macCatalyst)
             Text("\(memory)")
                 .accessibility(identifier: "memoryDisplay")
                 .padding(.horizontal, 5)
@@ -25,7 +32,24 @@ struct MemoryView: View {
                         .stroke(lineWidth: 2)
                         .foregroundColor(Color.gray)
                 )
+            // Add gesture here
+                .gesture(doubleTap)
+#else
+            Text("\(memory)")
+                .accessibility(identifier: "memoryDisplay")
+                .padding(.horizontal, 5)
+                .frame(
+                    width: geometry.size.width * 0.85,
+                    alignment: .trailing
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(Color.gray)
+                )
+            // Add gesture here
                 .gesture(memorySwipe)
+#endif
             Text("M")
         }.padding(.bottom).padding(.horizontal, 5)
     }
--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/04-testing-and-debugging/projects/starter/SwiftCalcUITests/SwiftCalcUITests.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/04-testing-and-debugging/projects/starter/SwiftCalcUITests/SwiftCalcUITests.swift
@@ -29,22 +29,22 @@
 import XCTest
 
 class SwiftCalcUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
     // MARK: - Test
-
+    
     func testPressMemoryPlusAtAppStartShowZeroInDisplay() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
@@ -79,7 +79,6 @@ class SwiftCalcUITests: XCTestCase {
         XCTAssert(displayText == "8.0")
     }
     
-#if !targetEnvironment(macCatalyst)
     func testSwipeToClearMemory() {
         let app = XCUIApplication()
         app.launch()
@@ -95,11 +94,14 @@ class SwiftCalcUITests: XCTestCase {
         let memoryDisplay = app.staticTexts["memoryDisplay"]
         
         XCTAssert(memoryDisplay.exists)
+#if targetEnvironment(macCatalyst)
+        memoryDisplay.doubleTap()
+#else
         memoryDisplay.swipeLeft()
+#endif
         XCTAssertFalse(memoryDisplay.exists)
     }
-#endif
-
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
