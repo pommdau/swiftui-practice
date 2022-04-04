bdea519c40e32107d7a868dc38696de75c3ff7a3
IKEH1024 <pondau1024@gmail.com>
2022-04-04 15:04:01 +0900

    keep reading

--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
@@ -9,16 +9,7 @@ struct RegisterView: View {
             Spacer()
             WelcomeMessageView()
             TextField("Type your name...", text: $name)
-                .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
-                .background(Color.white)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(lineWidth: 2)
-                        .foregroundColor(.blue)
-                )
-                .shadow(
-                    color: Color.gray.opacity(0.4),
-                    radius: 3, x: 1, y: 2)
+                .bordered()
             Spacer()
         }
         .padding()
