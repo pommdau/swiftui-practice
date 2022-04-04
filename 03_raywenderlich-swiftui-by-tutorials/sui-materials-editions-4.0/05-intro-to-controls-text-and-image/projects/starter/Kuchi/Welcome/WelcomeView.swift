830a1c1bd28a82858fed2a81895f3582c37ecf65
IKEH1024 <pondau1024@gmail.com>
2022-04-04 11:33:41 +0900

    keep reading

--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/05-intro-to-controls-text-and-image/projects/starter/Kuchi/Welcome/WelcomeView.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/05-intro-to-controls-text-and-image/projects/starter/Kuchi/Welcome/WelcomeView.swift
@@ -2,14 +2,33 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Image(systemName: "table")
-            .resizable()
-            .frame(width: 30, height: 30)
-            .cornerRadius(30 / 2)  // not needed
-            .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-            .background(Color(white: 0.9))
-            .clipShape(Circle())
-            .foregroundColor(.red)
+        ZStack {
+            Image("welcome-background", bundle: nil)
+                .resizable()
+//                .scaledToFit()
+                .aspectRatio(1 / 1, contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+                .saturation(0.5)
+                .blur(radius: 5)
+                .opacity(0.08)
+            
+            HStack {
+                Image(systemName: "table")
+                  .resizable()
+                  .frame(width: 30, height: 30)
+                  .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                  .background(Color(white: 0.9))
+                  .clipShape(Circle())
+                  .foregroundColor(.red)
+
+                Text("Welcome to Kuchi")
+                  .font(.system(size: 30))
+                  .bold()
+                  .foregroundColor(.red)
+                  .lineLimit(2)
+                  .multilineTextAlignment(.center)
+            }
+        }
     }
 }
 
