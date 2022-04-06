0e375e991f6fe15fd592e628b873b3f677121631
IKEH1024 <pondau1024@gmail.com>
2022-04-06 10:31:56 +0900

    keep reading

--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/Practice/ChallengeView.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/Practice/ChallengeView.swift
@@ -1,19 +1,43 @@
 import SwiftUI
 
 struct ChallengeView: View {
+    
+    let challengeTest: ChallengeTest
+    
+    @State var showAnswers = false
+    
     var body: some View {
-        HStack(alignment: .firstTextBaseline) {
-            Text("Welcome to Kuchi").font(.caption)
-            Text("Welcome to Kuchi").font(.title)
-            Button(action: {}, label: { Text("OK").font(.body) })
+        VStack {
+            Button(action: {
+                showAnswers.toggle()
+            }) {
+                QuestionView(question: challengeTest.challenge.question)
+                    .frame(height: 300)
+            }
+            if showAnswers {
+                Divider()
+                ChoicesView(challengeTest: challengeTest)
+                    .frame(height: 300)
+                    .padding()
+            }
         }
     }
-    
 }
 
 
 struct ChallengeView_Previews: PreviewProvider {
+    
+    static let challengeTest = ChallengeTest(
+        challenge: Challenge(
+            question: "おねがい　します",
+            pronunciation: "Onegai shimasu",
+            answer: "Please"
+        ),
+        answers: ["Thank you", "Hello", "Goodbye"]
+    )
+    
     static var previews: some View {
-        return ChallengeView()
+        return ChallengeView(challengeTest: challengeTest)
     }
+    
 }
