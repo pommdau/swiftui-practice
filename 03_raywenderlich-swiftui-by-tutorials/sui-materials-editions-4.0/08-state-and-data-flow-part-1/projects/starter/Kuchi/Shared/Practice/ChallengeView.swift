8dc09de5da111133b0571b0899595a42b75f68d4
IKEH1024 <pondau1024@gmail.com>
2022-04-08 16:08:04 +0900

    keep reading

--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/08-state-and-data-flow-part-1/projects/starter/Kuchi/Shared/Practice/ChallengeView.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/08-state-and-data-flow-part-1/projects/starter/Kuchi/Shared/Practice/ChallengeView.swift
@@ -4,16 +4,20 @@ struct ChallengeView: View {
     let challengeTest: ChallengeTest
     
     @State var showAnswers = false
+    @State var numberOfAnswered = 0
     
     var body: some View {
         VStack {
             Button(action: {
                 showAnswers.toggle()
+                self.numberOfAnswered += 1
             }) {
                 QuestionView(question: challengeTest.challenge.question)
                     .frame(height: 300)
             }
-            ScoreView(numberOfQuestions: 5)
+            ScoreView(numberOfQuestions: 5,
+                      numberOfAnswered: numberOfAnswered)
+            Text("ChallengeView Counter: \(numberOfAnswered)")
             
             if showAnswers {
                 Divider()
--- 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/08-state-and-data-flow-part-1/projects/starter/Kuchi/Shared/Practice/ScoreView.swift
+++ 03_raywenderlich-swiftui-by-tutorials/sui-materials-editions-4.0/08-state-and-data-flow-part-1/projects/starter/Kuchi/Shared/Practice/ScoreView.swift
@@ -4,20 +4,25 @@ import SwiftUI
 
 struct ScoreView: View {
     let numberOfQuestions: Int
-    @State var numberOfAnswered = 0
+    @State var numberOfAnswered: Int
     
     var body: some View {
         HStack {
             Text("\(numberOfAnswered)/\(numberOfQuestions)")
                 .font(.caption)
                 .padding(4)
+            Text("ScoreView Counter: \(numberOfAnswered)")
             Spacer()
         }
     }
 }
 
 struct ScoreView_Previews: PreviewProvider {
+    
+    @State static var numberOfAnswered: Int = 0
+    
     static var previews: some View {
-        ScoreView(numberOfQuestions: 5)
+        ScoreView(numberOfQuestions: 5,
+                  numberOfAnswered: numberOfAnswered)
     }
 }
