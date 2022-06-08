8315087af690e7be6586d373ee718337c899d9e3
IKEH1024 <pondau1024@gmail.com>
2022-06-08 15:24:05 +0900

    wip

--- 05_Zenn用/PanAndDismissDemo/PanAndDismissDemo/PanAndDismissModifier.swift
+++ 05_Zenn用/PanAndDismissDemo/PanAndDismissDemo/PanAndDismissModifier.swift
@@ -13,7 +13,7 @@ struct PanAndDismissModifier: ViewModifier {
     @Binding var showingModal: Bool
     @State private var imageDismissTransition: AnyTransition = .move(edge: .leading)
     @State private var backgroundColorOpacity: Double = 1.0
-        
+    
     func body(content: Content) -> some View {
         ZStack {
             if showingModal {
@@ -26,17 +26,17 @@ struct PanAndDismissModifier: ViewModifier {
             
             if showingModal {
                 content
-//                SwipableImageView(imageName: imageName,
-//                                  backgroundColorOpacity: $backgroundColorOpacity,
-//                                  imageDismissTransition: $imageDismissTransition,
-//                                  onDismiss: {
-//                    withAnimation {
-//                        showingModal = false
-//                    }
-//                })
-                .zIndex(1)
-                .transition(.asymmetric(insertion: .opacity,
-                                        removal: imageDismissTransition))
+                    .modifier(SwipableModifier(
+                        backgroundColorOpacity: $backgroundColorOpacity,
+                        imageDismissTransition: $imageDismissTransition,
+                        onDismiss: {
+                            withAnimation {
+                                showingModal = false
+                            }
+                        }))
+                    .zIndex(1)
+                    .transition(.asymmetric(insertion: .opacity,
+                                            removal: imageDismissTransition))
             }
         }
     }
@@ -44,8 +44,49 @@ struct PanAndDismissModifier: ViewModifier {
 
 struct SwipableModifier: ViewModifier {
     
+    @State private var offset: CGSize = .zero
+    @Binding var backgroundColorOpacity: Double
+    @Binding var imageDismissTransition: AnyTransition
+    
+    let dismissThreshold: CGFloat = 150
+    var onDismiss: () -> Void
+    
     func body(content: Content) -> some View {
-        
+        GeometryReader { geometry in
+            ZStack {
+                Color(uiColor: .clear)
+                    .contentShape(Rectangle())
+                    .ignoresSafeArea()
+                content
+                    .offset(y: offset.height)
+                    .animation(.interactiveSpring(), value: offset)
+            }
+            .position(x: geometry.frame(in: .local).midX, y: geometry.frame(in: .local).midY)
+            .simultaneousGesture(
+                DragGesture()
+                    .onChanged { gesture in
+                        if gesture.translation.width < 50 {
+                            offset = gesture.translation
+                        }
+                        // 画面の高さに対してどれだけスワイプされているかの比率
+                        backgroundColorOpacity = 1 - abs(Double(offset.height / geometry.size.height))
+                    }
+                    .onEnded { _ in
+                        if offset.height < -dismissThreshold {
+                            imageDismissTransition = .move(edge: .top)
+                            onDismiss()
+                            backgroundColorOpacity = 1.0
+                        } else if offset.height > dismissThreshold {
+                            imageDismissTransition = .move(edge: .bottom)
+                            onDismiss()
+                            backgroundColorOpacity = 1.0
+                        } else {
+                            offset = .zero
+                            backgroundColorOpacity = 1.0
+                        }
+                    }
+            )
+        }
     }
     
 }
