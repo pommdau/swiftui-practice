5241ffd11cbbd5a4b9e7cfa2059f97dab32acbc0
IKEH1024 <pondau1024@gmail.com>
2022-06-08 22:19:27 +0900

    wip

--- 99_Instance/ZoomableViewDemo/ZoomableViewDemo/DetailView.swift
+++ 99_Instance/ZoomableViewDemo/ZoomableViewDemo/DetailView.swift
@@ -32,16 +32,62 @@ struct ZoomableView_Previews: PreviewProvider {
 
 public struct IKEHZoomableModifier: ViewModifier {
 
-    // MARK: - Properties
+    // MARK: - Enum
+    
+    private enum ZoomState {
+        case inactive
+        case active(scale: CGFloat)
         
+        var scale: CGFloat {
+            switch self {
+            case .active(let scale):
+                return scale
+            default: return 1.0
+            }
+        }
+    }
+    
+    // MARK: - Properties
+    
     var showsIndicators: Bool = true
     var screenSize: CGSize = .zero
+    var min: CGFloat = 1.0  // TODO: RENAME
+    var max: CGFloat = 2.0
+    @GestureState private var zoomState = ZoomState.inactive  // ズーム中のスケール
+    @State private var currentScale: CGFloat = 1.0  // 現在のスケール
+
+    var trueScale: CGFloat {
+        return currentScale * zoomState.scale
+    }
     
     // MARK: - Computed Properties
     
     var scrollViewAxes: Axis.Set {
 //        return scale > 1.0 ? [.horizontal, .vertical] : []
-        return [.horizontal, .vertical]
+        return  [.horizontal, .vertical]
+    }
+    
+    var zoomGesture: some Gesture {
+        MagnificationGesture()
+            .updating($zoomState) { value, referedZoomState, transaction in
+                referedZoomState = .active(scale: value)
+            }
+            .onEnded { value in
+                print("onEnded")
+                var new = self.currentScale * value
+                if new <= min { new = min }
+                if new >= max { new = max }
+                self.currentScale = new
+            }
+    }
+    
+    var doubleTapGesture: some Gesture {
+        TapGesture(count: 2).onEnded {
+            if trueScale <= min { currentScale = max } else
+            if trueScale >= max { currentScale = min } else {
+                currentScale = ((max - min) * 0.5 + min) < trueScale ? max : min
+            }
+        }
     }
     
     // MARK: - LifeCycle
@@ -52,7 +98,10 @@ public struct IKEHZoomableModifier: ViewModifier {
         ScrollView(scrollViewAxes, showsIndicators: showsIndicators) {
             content
                 .frame(maxWidth: screenSize.width, maxHeight: screenSize.height)
+                .scaleEffect(trueScale, anchor: .center)
         }
+        .gesture(ExclusiveGesture(zoomGesture, doubleTapGesture))
         .background(.blue)
+        .animation(.easeInOut, value: trueScale)
     }
 }
