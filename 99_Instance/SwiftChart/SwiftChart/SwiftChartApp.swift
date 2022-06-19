696a6c4c03fcf8d253bc736ea042dde943f43fe7
IKEH1024 <pondau1024@gmail.com>
2022-06-19 23:14:33 +0900

    wip

---
+++ 99_Instance/SwiftChart/SwiftChart/SwiftChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartApp.swift
+//  SwiftChart
+//
+//  Created by HIROKI IKEUCHI on 2022/06/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
