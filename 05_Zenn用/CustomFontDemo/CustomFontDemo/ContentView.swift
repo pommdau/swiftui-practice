0cae9b9f54c3d93cd59167e39699f4a8ea9d19b4
IKEH1024 <pondau1024@gmail.com>
2022-06-30 14:34:39 +0900

    create project

---
+++ 05_Zenn用/CustomFontDemo/CustomFontDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomFontDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/06/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
