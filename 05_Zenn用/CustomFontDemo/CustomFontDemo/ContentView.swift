ef091af286b7a03102b56e447f698a76f037b30d
IKEH1024 <pondau1024@gmail.com>
2022-06-30 17:07:50 +0900

    wip

--- 05_Zenn用/CustomFontDemo/CustomFontDemo/ContentView.swift
+++ 05_Zenn用/CustomFontDemo/CustomFontDemo/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var fontURLs: CFArray {
+        
+        let fontNames: [String] = ["", "", ""]
+        
+        let fontURLs: [URL] = fontNames.compactMap { fontName in
+            if let resource = Bundle.main.resourceURL {
+                return resource.appendingPathComponent(fontName)
+            } else {
+                return nil
+            }
+        }
+        
+        return fontURLs as CFArray
+    }
+           
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+                        
+            Text("Hello, world!")
+                .padding()
+            Button {
+                                
+                CTFontManagerRegisterFontURLs(self.fontURLs, .user, true) { cfarray, result in
+                    print(cfarray)
+                    print(result)
+                    return true
+                }
+                
+//                CTFontManagerRegisterFontsWithAssetNames(
+//                    ["NikkyouSans-mLKax"] as CFArray,
+//                    nil, .user, true, { arr, result in
+//                        print(arr)
+//                        print(result)
+//                        return true
+//                    })
+            } label: {
+                Text("Install font")
+            }
+            
+        }
+        
+        
     }
 }
 
