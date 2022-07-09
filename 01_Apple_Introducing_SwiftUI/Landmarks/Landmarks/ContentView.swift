b01ddfac372415448dad3a3fe25a1080a4423bba
IKEH1024 <pondau1024@gmail.com>
2022-07-10 00:36:42 +0900

    initialize project

---
+++ 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by HIROKI IKEUCHI on 2022/07/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/LandmarksApp.swift
+++ 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/LandmarksApp.swift
@@ -2,20 +2,16 @@
 //  LandmarksApp.swift
 //  Landmarks
 //
-//  Created by HIROKI IKEUCHI on 2022/02/27.
+//  Created by HIROKI IKEUCHI on 2022/07/10.
 //
 
 import SwiftUI
 
-@main  // @main: entry point identifier
+@main
 struct LandmarksApp: App {
-    @StateObject private var modelData = ModelData()
-    
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(modelData)
         }
     }
 }
