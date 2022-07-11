027b44a77ca774f6f3bb67a15bed2f48c8f4e76f
IKEH1024 <pondau1024@gmail.com>
2022-07-11 16:29:33 +0900

    wip

--- 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/Views/LandmarkList.swift
+++ 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -24,6 +24,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)  // プレビューにラベル付け
+        }
     }
 }
