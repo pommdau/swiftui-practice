7ba7929e6c07eaa517ad0d2eb05edc16ae687b17
IKEH1024 <pondau1024@gmail.com>
2022-07-11 16:48:47 +0900

    wip

--- 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/Views/LandmarkList.swift
+++ 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct LandmarkList: View {
     
-    @State private var showFavoritesOnly = true
+    @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
@@ -19,11 +19,18 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
