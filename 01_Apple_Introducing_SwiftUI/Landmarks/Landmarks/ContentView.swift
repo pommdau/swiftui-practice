3e30aa474efe95dc8b823eb956c81ba7852819e7
IKEH1024 <pondau1024@gmail.com>
2022-07-11 14:35:49 +0900

    keep reading

--- 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/ContentView.swift
+++ 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
