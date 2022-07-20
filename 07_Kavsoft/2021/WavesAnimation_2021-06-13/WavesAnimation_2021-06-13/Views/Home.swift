22e647aa5b1ce15dfacd3c9c6313ae24ec7daf9a
IKEH1024 <pondau1024@gmail.com>
2022-07-21 01:49:42 +0900

    wip

---
+++ 07_Kavsoft/2021/WavesAnimation_2021-06-13/WavesAnimation_2021-06-13/Views/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  WavesAnimation_2021-06-13
+//
+//  Created by HIROKI IKEUCHI on 2022/07/21.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
