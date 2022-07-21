695a2eaf5fc1980db3f7f914b3f6963f059025ba
IKEH1024 <pondau1024@gmail.com>
2022-07-21 09:46:39 +0900

    wip

--- 07_Kavsoft/2021/WavesAnimation_2021-06-13/README.md
+++ 07_Kavsoft/2021/WavesAnimation_2021-06-13/README.md
@@ -2,4 +2,9 @@
 ## Ref
 - [SwiftUI 3\.0 \- Wave's Animation \- TimeLineView \+ Canvas View \- Wave Form \- Xcode 13 WWDC 2021](https://www.youtube.com/watch?v=qHGpf4fmjrA)
 - [swiftUIのCanvasを使ってみる](https://tech.unifa-e.com/entry/2021/12/20/000000)
+- [\[Swift\] あると便利だったextension達 剰余算編](https://dev.classmethod.jp/articles/utility-extension-remainder/)
+    - `remainder`の挙動
+- [translateBy\(x:y:\)](https://developer.apple.com/documentation/swiftui/graphicscontext/translateby(x:y:))
+
+>Moves subsequent drawing operations by an amount in each dimension.
 
--- 07_Kavsoft/2021/WavesAnimation_2021-06-13/WavesAnimation_2021-06-13/Views/Home.swift
+++ 07_Kavsoft/2021/WavesAnimation_2021-06-13/WavesAnimation_2021-06-13/Views/Home.swift
@@ -13,7 +13,7 @@ struct Home: View {
             // Wave form view...
             WaveForm(color: .purple, amplify: 150)
         }
-        .ignoresSafeArea(.all, edges: .bottom)        
+        .ignoresSafeArea(.all, edges: .bottom)
     }
 }
 
@@ -21,6 +21,7 @@ struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
             .previewDevice("iPhone 13 Pro Max")
+            .previewInterfaceOrientation(.portrait)
     }
 }
 
@@ -38,27 +39,61 @@ struct WaveForm: View {
             Canvas { context, size in
                 // getting current time
                 let timeNow = timeLine.date.timeIntervalSinceReferenceDate
+                                
+                // animating the wave using current time...
+                let angle = timeNow.remainder(dividingBy: 2)  // 2で割ったときの余り(純粋な余りではなく、飛び越えて負の値にもなる)
+                // calculating offset
+                let offset = angle * size.width
+                
+                context.draw(Text("\(angle)"),
+                             at: CGPoint(x: size.width / 2, y: 200))
+                context.draw(Text("\(offset)"),
+                             at: CGPoint(x: size.width / 2, y: 100))
+                
+                // you can see it now shifts to screen width...
+                
+                // you can see it moves between -1.5 - 1.5...
+                // ie 3/2 = 1.5
+                // if 2 means -1 to 1...
+                                
+                // moving the whole view...
+                // simple and easy wave animation
+                context.translateBy(x: offset, y: 0)
                 
                 // Using SwiftUI path for drawing wave...
-                context.fill(
-                    Path { path in
-                        let midHeight = size.height / 2
-                        let width = size.width
-                        
-                        // moving the wave to center leading
-                        path.move(to: CGPoint(x: 0, y: midHeight))
-                                                
-                        // drawing curve...
-                        path.addCurve(
-                            to: CGPoint(x: width, y: midHeight),
-                            control1: CGPoint(x: width * 0.5, y: midHeight + amplify),
-                            control2: CGPoint(x: width * 0.5, y: midHeight - amplify))
-                        
-                        // filling the bottom remaining
-                        path.addLine(to: CGPoint(x: width, y: size.height))
-                        path.addLine(to: CGPoint(x: 0, y: size.height))
-                    }, with: .color(color))
+                context.fill(getPath(size: size), with: .color(color))
+                
+                // drawing curve front and back
+                // so that translation will be look like wave animation...
+                context.translateBy(x: -size.width, y: 0)
+                
+                context.fill(getPath(size: size), with: .color(color))
+                
+                context.translateBy(x: size.width * 2, y: 0)
+                
+                context.fill(getPath(size: size), with: .color(color))
+                
             }
+        }
+    }
+    
+    private func getPath(size: CGSize) -> Path {
+        return Path { path in
+            let midHeight = size.height / 2
+            let width = size.width
+            
+            // moving the wave to center leading
+            path.move(to: CGPoint(x: 0, y: midHeight))
+            
+            // drawing curve...
+            path.addCurve(
+                to: CGPoint(x: width, y: midHeight),
+                control1: CGPoint(x: width * 0.5, y: midHeight + amplify),
+                control2: CGPoint(x: width * 0.5, y: midHeight - amplify))
+            
+            // filling the bottom remaining
+            path.addLine(to: CGPoint(x: width, y: size.height))
+            path.addLine(to: CGPoint(x: 0, y: size.height))
         }
     }
 }
