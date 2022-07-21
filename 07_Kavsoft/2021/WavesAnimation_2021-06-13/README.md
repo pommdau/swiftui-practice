db4a7ead95212ed3432d907680fd3f5429a29aeb
IKEH1024 <pondau1024@gmail.com>
2022-07-21 09:07:30 +0900

    wip

---
+++ 07_Kavsoft/2021/WavesAnimation_2021-06-13/README.md
@@ -0,0 +1,5 @@
+# WaveAnimation
+## Ref
+- [SwiftUI 3\.0 \- Wave's Animation \- TimeLineView \+ Canvas View \- Wave Form \- Xcode 13 WWDC 2021](https://www.youtube.com/watch?v=qHGpf4fmjrA)
+- [swiftUIのCanvasを使ってみる](https://tech.unifa-e.com/entry/2021/12/20/000000)
+
--- 07_Kavsoft/2021/WavesAnimation_2021-06-13/WavesAnimation_2021-06-13/Views/Home.swift
+++ 07_Kavsoft/2021/WavesAnimation_2021-06-13/WavesAnimation_2021-06-13/Views/Home.swift
@@ -9,12 +9,56 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Wave form view...
+            WaveForm(color: .purple, amplify: 150)
+        }
+        .ignoresSafeArea(.all, edges: .bottom)        
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+            .previewDevice("iPhone 13 Pro Max")
+    }
+}
+
+struct WaveForm: View {
+    
+    var color: Color
+    var amplify: CGFloat
+    
+    var body: some View {
+        // Using time line view for peroidc updates...
+        
+        TimelineView(.animation) { timeLine in
+            
+            // Canvas View for drawding wave...
+            Canvas { context, size in
+                // getting current time
+                let timeNow = timeLine.date.timeIntervalSinceReferenceDate
+                
+                // Using SwiftUI path for drawing wave...
+                context.fill(
+                    Path { path in
+                        let midHeight = size.height / 2
+                        let width = size.width
+                        
+                        // moving the wave to center leading
+                        path.move(to: CGPoint(x: 0, y: midHeight))
+                                                
+                        // drawing curve...
+                        path.addCurve(
+                            to: CGPoint(x: width, y: midHeight),
+                            control1: CGPoint(x: width * 0.5, y: midHeight + amplify),
+                            control2: CGPoint(x: width * 0.5, y: midHeight - amplify))
+                        
+                        // filling the bottom remaining
+                        path.addLine(to: CGPoint(x: width, y: size.height))
+                        path.addLine(to: CGPoint(x: 0, y: size.height))
+                    }, with: .color(color))
+            }
+        }
     }
 }
