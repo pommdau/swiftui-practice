c86cd6b703923142231de85b9e6d27687d3f357b
IKEH1024 <pondau1024@gmail.com>
2022-07-22 13:18:10 +0900

    add resources

---
+++ 06_単発/SpriteKit/README.md
@@ -0,0 +1,3 @@
+## SpriteKit
+## [SpriteKit Tutorial for Beginners](https://www.raywenderlich.com/71-spritekit-tutorial-for-beginners)
+
---
+++ 06_単発/SpriteKit/SpriteKit Tutorial for Beginners/NinjaAttack/NinjaAttackFinal/NinjaAttack/GameOverScene.swift
@@ -0,0 +1,67 @@
+/// Copyright (c) 2018 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Foundation
+import SpriteKit
+
+class GameOverScene: SKScene {
+  init(size: CGSize, won:Bool) {
+    super.init(size: size)
+    
+    // 1
+    backgroundColor = SKColor.white
+    
+    // 2
+    let message = won ? "You Won!" : "You Lose :["
+    
+    // 3
+    let label = SKLabelNode(fontNamed: "Chalkduster")
+    label.text = message
+    label.fontSize = 40
+    label.fontColor = SKColor.black
+    label.position = CGPoint(x: size.width/2, y: size.height/2)
+    addChild(label)
+    
+    // 4
+    run(SKAction.sequence([
+      SKAction.wait(forDuration: 3.0),
+      SKAction.run() { [weak self] in
+        // 5
+        guard let `self` = self else { return }
+        let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+        let scene = GameScene(size: size)
+        self.view?.presentScene(scene, transition:reveal)
+      }
+      ]))
+  }
+  
+  // 6
+  required init(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
