202e42cacf043405e209628729c795569bbf09d8
IKEH1024 <pondau1024@gmail.com>
2022-07-22 13:27:31 +0900

    wip

--- 06_単発/SpriteKit/SpriteKit Tutorial for Beginners/NinjaAttack/NinjaAttackStarter/NinjaAttack/GameScene.swift
+++ 06_単発/SpriteKit/SpriteKit Tutorial for Beginners/NinjaAttack/NinjaAttackStarter/NinjaAttack/GameScene.swift
@@ -30,13 +30,67 @@ import SpriteKit
 
 class GameScene: SKScene {
   
+  // MARK: - Properties
+  
   let player = SKSpriteNode(imageNamed: "player")
+  
+  // MARK: - Overrides
   
   override func didMove(to view: SKView) {
     backgroundColor = SKColor.white
     player.position = CGPoint(x: size.width * 0.1,
                               y: size.height * 0.5)
     addChild(player)
+    
+    run(SKAction.repeatForever(
+      SKAction.sequence([
+        SKAction.run(addMonster),
+        SKAction.wait(forDuration: 1.0)
+      ])
+    ))
+  }
+  
+}
+
+// MARK: - Helpers
+
+extension GameScene {
+  func random() -> CGFloat {
+    return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+  }
+  
+  func random(min: CGFloat, max: CGFloat) -> CGFloat {
+    return random() * (max - min) + min
+  }
+  
+  func addMonster() {
+    
+    // Create sprite
+    let monster = SKSpriteNode(imageNamed: "monster")
+    
+    // Determine where to spawn the monster along the Y axis
+    let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+    
+    // Position the monster slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+    
+    // Add the monster to the scene
+    addChild(monster)
+    
+    // Determine speed of the monster
+    let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+    
+    // Create the actions
+    // duration: 移動にかかる時間
+    let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: actualY),
+                                   duration: TimeInterval(actualDuration))
+    
+    // removeFromParent: parentからnodeを削除
+    let actionMoveDone = SKAction.removeFromParent()
+    
+    // sequence: SKActionを順番に連続して処理させる
+    monster.run(SKAction.sequence([actionMove, actionMoveDone]))
   }
   
 }
