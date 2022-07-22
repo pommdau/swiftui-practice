5fd4d637ed656b7db28d540c1da80d5ec8505d64
IKEH1024 <pondau1024@gmail.com>
2022-07-22 13:49:50 +0900

    wip

---
+++ 06_単発/SpriteKit/SpriteKit Tutorial for Beginners/NinjaAttack/NinjaAttackStarter/GameScene+SKPhysicsContactDelegate.swift
@@ -0,0 +1,7 @@
+import SpriteKit
+
+// MARK: - SKPhysicsContactDelegate
+
+extension GameScene: SKPhysicsContactDelegate {
+  
+}
--- 06_単発/SpriteKit/SpriteKit Tutorial for Beginners/NinjaAttack/NinjaAttackStarter/GameScene.swift
+++ 06_単発/SpriteKit/SpriteKit Tutorial for Beginners/NinjaAttack/NinjaAttackStarter/GameScene.swift
@@ -1,31 +1,3 @@
-/// Copyright (c) 2018 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import SpriteKit
 
 class GameScene: SKScene {
@@ -41,6 +13,9 @@ class GameScene: SKScene {
     player.position = CGPoint(x: size.width * 0.1,
                               y: size.height * 0.5)
     addChild(player)
+    
+    physicsWorld.gravity = .zero  // 重力なしの設定
+    physicsWorld.contactDelegate = self
     
     run(SKAction.repeatForever(
       SKAction.sequence([
@@ -106,6 +81,11 @@ extension GameScene {
     
     // Create sprite
     let monster = SKSpriteNode(imageNamed: "monster")
+    monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size) // 1: Sprite用の長方形を当たり判定を定義
+    monster.physicsBody?.isDynamic = true // 2: 物理エンジンにモンスターの動きを制御させない
+    monster.physicsBody?.categoryBitMask = PhysicsCategory.monster // 3: カテゴリマスクの設定
+    monster.physicsBody?.contactTestBitMask = PhysicsCategory.projectile // 4:指定したカテゴリのオブジェクトと交差したときに通知する設定
+    monster.physicsBody?.collisionBitMask = PhysicsCategory.none // 5: 当たり判定を無視する設定
     
     // Determine where to spawn the monster along the Y axis
     let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
