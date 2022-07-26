a9ae9ceeae00ef9d50480785269fb5233cfbc14b
IKEH1024 <pondau1024@gmail.com>
2022-07-26 22:43:29 +0900

    wip

--- 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/ContentView.swift
+++ 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            NoSpringView()
+                .padding()
+                .tabItem {
+                    Image(systemName: "1.circle.fill")
+                    Text("Linear")
+                }
+        }
     }
 }
 
---
+++ 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/VIews/NoSpringView.swift
@@ -0,0 +1,20 @@
+//
+//  NoSpringView.swift
+//  SpringAnimationDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/07/26.
+//
+
+import SwiftUI
+
+struct NoSpringView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoSpringView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoSpringView()
+    }
+}
