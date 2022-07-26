8564e0a4b962ec09813a67e62b18e08e4d418596
IKEH1024 <pondau1024@gmail.com>
2022-07-26 23:54:46 +0900

    wip

--- 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/ContentView.swift
+++ 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/ContentView.swift
@@ -8,16 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    private let standardHeight: CGFloat = 400
+    
     var body: some View {
-        TabView {
-            NoSpringView()
-                .padding()
-                .tabItem {
-                    Image(systemName: "1.circle.fill")
-                    Text("Linear")
-                }
+
+        GeometryReader { geomerry in
+            Path { path in
+                path.move(to: CGPoint(x: 0, y: standardHeight))
+                path.addLine(to: CGPoint(x: geomerry.size.width,
+                                         y: standardHeight))
+            }
+            .stroke(lineWidth: 2)
+            .foregroundColor(.gray.opacity(0.3))
         }
+        
+        
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
