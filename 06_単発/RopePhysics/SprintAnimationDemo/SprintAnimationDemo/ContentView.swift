3d7b2b86668a66523f0a1dc466f4537e4b865442
IKEH1024 <pondau1024@gmail.com>
2022-07-26 22:32:51 +0900

    create project

---
+++ 06_単発/RopePhysics/SprintAnimationDemo/SprintAnimationDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SprintAnimationDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 06_単発/RopePhysics/SprintAnimationDemo/SprintAnimationDemo/SprintAnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprintAnimationDemoApp.swift
+//  SprintAnimationDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SprintAnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
