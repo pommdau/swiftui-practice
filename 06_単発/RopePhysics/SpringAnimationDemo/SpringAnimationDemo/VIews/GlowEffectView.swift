ab5fc7fe12070aac0b3c46b9bc10c63ad3b64fa6
IKEH1024 <pondau1024@gmail.com>
2022-07-27 22:13:23 +0900

    wip

--- 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/VIews/GlowEffectView.swift
+++ 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/VIews/GlowEffectView.swift
@@ -21,6 +21,38 @@ struct GlowEffectView: View {
     private let lineWidth: CGFloat = 8
     private let dashPattern: [CGFloat] = [12, 14]
     
+    private var LinePath: Path {
+        Path { path in
+            path.move(to: CGPoint(x: 10, y: 400))
+            path.addLine(to: CGPoint(x: 400, y: 400))
+        }
+    }
+    
+    @ViewBuilder
+    private func Rope() -> some View {
+        ZStack {
+            LinePath
+                .stroke(lineWidth: lineWidth)
+                .foregroundColor(.white)
+                .blur(radius: isGlowing ? 10 : 0)
+            
+            LinePath
+                .stroke(.white, lineWidth: lineWidth)
+                .foregroundColor(.white)
+            
+            LinePath
+                .stroke(style: StrokeStyle(
+                    lineWidth: lineWidth,
+                    dash: dashPattern,
+                    dashPhase: dashPhase))
+                .foregroundColor(.blue)
+                .onReceive(timer) { _ in
+                    timerCount = timerCount > dashPattern.reduce(0){ $0 + $1 } ? 0 : timerCount + 1
+                    dashPhase = timerCount
+                }
+        }
+    }
+    
     var body: some View {
         ZStack {
             Color.gray
@@ -45,62 +77,7 @@ struct GlowEffectView: View {
                         .foregroundColor(.white)
                 }
                 
-                ZStack {
-//                    Path { path in
-//                        path.move(to: CGPoint(x: 10, y: 400))
-//                        path.addLine(to: CGPoint(x: 400, y: 400))
-//                    }
-//                    .stroke(lineWidth: 20)
-//                    .foregroundColor(.white)
-//                    .blur(radius: isGlowing ? 30.0 : 0)
-
-                    Path { path in
-                        path.move(to: CGPoint(x: 10, y: 400))
-                        path.addLine(to: CGPoint(x: 400, y: 400))
-                    }
-                    .stroke(.white, lineWidth: lineWidth)
-                    .foregroundColor(.white)
-                    
-                    Path { path in
-                        path.move(to: CGPoint(x: 10, y: 400))
-                        path.addLine(to: CGPoint(x: 400, y: 400))
-                    }
-                    .stroke(style: StrokeStyle(
-                        lineWidth: lineWidth,
-                        dash: dashPattern,
-                        dashPhase: dashPhase))
-                    .foregroundColor(.blue)
-                    .onReceive(timer) { _ in
-                        timerCount = timerCount > dashPattern.reduce(0){ $0 + $1 } ? 0 : timerCount + 1
-                        dashPhase = timerCount
-                    }
-                    
-                    ZStack {
-                                                
-//                        Circle()
-//                            .stroke(lineWidth: 10)
-//                            .foregroundColor(.white)
-//                            .frame(width: 200, height: 200)
-//                            .onReceive(timer) { _ in
-//                                timerCount = timerCount > 10 ? 0 : timerCount + 1
-//                                dashPhase = timerCount
-//                            }
-                        
-//                        Circle()
-//                            .stroke(style: StrokeStyle(
-//                                lineWidth: 10,
-//                                dash: [20, 24],
-//                                dashPhase: dashPhase))
-//                            .foregroundColor(.red)
-//                            .frame(width: 200, height: 200)
-//                            .onReceive(timer) { _ in
-//                                timerCount = timerCount > 44 ? 0 : timerCount + 1
-//                                dashPhase = timerCount
-//                            }
-                        
-                    }
-                }
-                
+                Rope()
             }
         }
     }
