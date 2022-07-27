9e2baea4ce3cfb1d7322bea8b2ed7c91415427cc
IKEH1024 <pondau1024@gmail.com>
2022-07-27 10:40:41 +0900

    wip

--- 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/ContentView.swift
+++ 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/ContentView.swift
@@ -7,8 +7,23 @@
 
 import SwiftUI
 
+struct Point {
+    let mass: Double = 1  // 質量
+    var x: Double = 100
+    var v: Double = 0
+}
+
+struct Spring {
+    let springLength: Double = 0
+    var k: Double = -20  // stiffness: 20
+    let d = -0.5  // damping: 減衰振動
+//    let d: Double = 0.0
+}
+
 class PhysicsManager: ObservableObject {
     private var timer: Timer? = nil
+    var point = Point()
+    private let spring = Spring()
     
     func startTimer() {
         timer = Timer.scheduledTimer(withTimeInterval: frameRate, repeats: true) { _ in
@@ -20,23 +35,15 @@ class PhysicsManager: ObservableObject {
         timer?.invalidate()
         timer = nil
     }
-    
-    let mass: Double = 1  // 質量
-    let springLength: Double = 0
-    var x: Double = 100
-    var v: Double = 0
-    var k: Double = -20  // stiffness: 20
-    let d = -0.5  // damping: 減衰振動
-//    let d: Double = 0.0
+                    
     var frameRate: Double = 1 / 60
-    var positions = [CGFloat]()
     
     private func updateStatus() {
-        let fSpring = k * (x - springLength)  // フックの法則
-        let fDamping = d * v
-        let a = (fSpring + fDamping) / mass  // 加速度
-        v = v + a * frameRate
-        x = x + v * frameRate  // x=v0t+1/2at^2ではなく前の位置を使用する(同じ意味ではある)
+        let fSpring = spring.k * (point.x - spring.springLength)  // フックの法則
+        let fDamping = spring.d * point.v
+        let a = (fSpring + fDamping) / point.mass  // 加速度
+        point.v = point.v + a * frameRate
+        point.x = point.x + point.v * frameRate  // x=v0t+1/2at^2ではなく前の位置を使用する(同じ意味ではある)
     }
     
 }
@@ -50,11 +57,11 @@ struct ContentView: View {
             
             VStack {
                 
-                Text("\(physicsManager.x)")
+                Text("\(physicsManager.point.x)")
                 
                 ZStack {
                     Circle()
-                        .position(CGPoint(x: 10, y: 200 + physicsManager.x))
+                        .position(CGPoint(x: 10, y: 200 + physicsManager.point.x))
                         .frame(width: 30, height: 30)
                         .foregroundColor(.red)
                     Circle()
