ead1ab8c5caced76bcbd5b3d3970c80e184a73ad
IKEH1024 <pondau1024@gmail.com>
2022-07-28 16:36:57 +0900

    wip

---
+++ 06_単発/CustomFontDemo/CustomFontDemo/SplitViewDemo.swift
@@ -0,0 +1,74 @@
+//
+//  SplitViewDemo.swift
+//  CustomFontDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/07/28.
+//
+
+import SwiftUI
+
+struct SplitViewDemo: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Actions").textCase(.none)) {
+                    Button {
+                    } label: {
+                        Text("Install all")
+                    }
+                    
+                    Button {
+                    } label: {
+                        Text("Uninstall all")
+                    }
+                    
+                    Button {
+                    } label: {
+                        Text("Open Settings...")
+                    }
+                }
+                Section(header: Text("Fonts").textCase(.none)) {
+                    
+                    NavigationLink {
+                        Image(systemName: "camera.macro")
+                            .resizable()
+                            .frame(width: 300, height: 300)
+                    } label: {
+                        FontRow(name: "Font1")
+                    }
+                    
+                    NavigationLink {
+                        Image(systemName: "key.viewfinder")
+                            .resizable()
+                            .frame(width: 300, height: 300)
+                    } label: {
+                        Text("Font2")
+                    }
+                    
+                }                
+            }
+        }
+        .padding(0.5)
+    }
+}
+
+struct FontRow: View {
+    
+    var name: String
+    
+    var body: some View {
+        HStack {
+            Text(name)
+            Spacer()
+            Image(systemName: "star.fill")
+                .foregroundColor(.yellow)
+        }
+    }
+}
+
+struct SplitViewDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        SplitViewDemo()
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
