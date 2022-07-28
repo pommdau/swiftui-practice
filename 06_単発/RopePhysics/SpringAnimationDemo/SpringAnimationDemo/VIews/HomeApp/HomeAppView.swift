a6972ecd99b1fb7343298bc3690a1645dbd4f39f
IKEH1024 <pondau1024@gmail.com>
2022-07-28 12:51:28 +0900

    wip

--- 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/VIews/HomeApp/HomeAppView.swift
+++ 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/VIews/HomeApp/HomeAppView.swift
@@ -9,26 +9,42 @@ import SwiftUI
 
 
 struct HomeAppView: View {
+    
+    @State private var anchorFrames: [CGRect] = [CGRect(x: 100, y: 100, width: 100, height: 100)]
     @State private var rectangleUnitFrames = [CGRect]()
     @State private var rectangleUnitStates = [false]
     
     var body: some View {
-        HStack(spacing: 20) {
-//            Toggle(isOn: $isActive.animation()) {}
-            RectangleUnitView(color: .blue, active: $rectangleUnitStates[0])
-                .overlay(
-                    GeometryReader { geo in
-                        Color.clear
-                            .onAppear {
-                                rectangleUnitFrames.insert(
-                                    (geo.frame(in: .global)), at: 0
-                                )
-                            }
-                    }
-                )
+                 
+        ZStack {
+            GeometryReader { geo in
+                Circle()
+                    .frame(width: 20, height: 20)
+                    .position(CGPoint(
+                        x: anchorFrames[0].origin.x - geo.frame(in: .global).origin.x,
+                        y: anchorFrames[0].origin.y - geo.frame(in: .global).origin.y)
+                    )
+                    .foregroundColor(.orange)
+            }
+            
+            HStack(spacing: 20) {
+    //            Toggle(isOn: $isActive.animation()) {}
+                RectangleUnitView(color: .blue, active: $rectangleUnitStates[0])
+                    .overlay(
+                        GeometryReader { geo in
+                            Color.clear
+                                .onAppear {
+                                    rectangleUnitFrames.insert(
+                                        (geo.frame(in: .global)), at: 0
+                                    )
+                                }
+                        }
+                    )
+            }
         }
         .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .global)
             .onChanged({ (value) in
+                anchorFrames[0].origin = value.location                
                 if let match = rectangleUnitFrames.firstIndex(where: { frame in
                     let length: CGFloat = 40  // ニコちゃんマークの大きさと一致させる
                     let validFrame = CGRect(origin: CGPoint(x: frame.midX - length / 2,
