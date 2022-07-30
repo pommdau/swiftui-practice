adbc23ffcbdf0b749a5641e95c98898bc7e3a9b4
IKEH1024 <pondau1024@gmail.com>
2022-07-30 13:17:37 +0900

    wip

---
+++ 06_単発/RopePhysics/SpringAnimationDemo/SpringAnimationDemo/Extensions/UnitColors.swift
@@ -0,0 +1,25 @@
+//
+//  UnitColors.swift
+//  SpringAnimationDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/07/29.
+//
+
+import SwiftUI
+
+struct UnitColors {
+    let frameStroke: Color
+    let frameFill: Color
+    let iconStroke: Color
+    let iconFill: Color
+}
+
+extension UnitColors {
+    static let unit1 = UnitColors(frameStroke: Color(red: 0.244, green: 0.551, blue: 0.950),
+                                 frameFill: Color(red: 0.373, green: 0.659, blue: 0.986),
+                                 iconStroke: Color(red: 0.109, green: 0.271, blue: 0.730),
+                                 iconFill: Color(red: 0.152, green: 0.377, blue: 0.903))
+    
+    
+    
+}
