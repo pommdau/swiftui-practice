9212b63e772e42b7c675d9481008dec3a6a86291
IKEH1024 <pondau1024@gmail.com>
2022-07-31 13:38:04 +0900

    wip

--- 06_単発/RopePhysics/HomeAppDemo/HomeAppDemo/HomeAppView.swift
+++ 06_単発/RopePhysics/HomeAppDemo/HomeAppDemo/HomeAppView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct HomeAppView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RopeView(color: .blue, isGlowing: true)
     }
 }
 
--- 06_単発/RopePhysics/HomeAppDemo/HomeAppDemo/RopeView.swift
+++ 06_単発/RopePhysics/HomeAppDemo/HomeAppDemo/RopeView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct RopeView: View {
     
+    private let lineWidth: CGFloat = 8
+    private let dashPattern: [CGFloat] = [12, 14]
     
+    /// 破線のスタート地点を変更する為のプロパティ
+    @State private var dashPhase: CGFloat = 0
+    /// Timerのカウントを保持するプロパティ
+    @State private var timerCount: CGFloat = 0
+    /// 0.1秒毎に`dashPhase`を変更処理を実行する為のTimer
+    private let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    
+    let color: Color
+    @State var isGlowing: Bool
     
     var body: some View {
         TimelineView(.periodic(from: Date(), by: 1/60)) { context in
@@ -19,25 +30,65 @@ struct RopeView: View {
     
     @ViewBuilder
     private func Rope() -> some View {
+        
+        ZStack {
+             
+            // DEBUGGING
+            Button {
+                withAnimation {
+                    isGlowing.toggle()
+                }
+            } label: {
+                Text("Growing")
+            }
+                                        
+            // ロープの外枠
+            RopePath
+                .stroke(isGlowing ? color : Color(uiColor: #colorLiteral(red: 0.4756349325, green: 0.4756467342, blue: 0.4756404161, alpha: 1)),
+                        lineWidth: lineWidth + 4)
+                .shadow(color: .black.opacity(1.0), radius: 8, x: 0, y: 15)
+            
+            // ロープの中の色
+            RopePath
+                .stroke(isGlowing ? .white : .gray,
+                        lineWidth: lineWidth)
+            
+            if isGlowing {
+                // Glowing effect
+                RopePath
+                    .stroke(lineWidth: lineWidth)
+                    .foregroundColor(.white)
+                    .blur(radius: lineWidth + 2)
+                    .zIndex(-1)
+                    
+                // 点線の移動
+                RopePath
+                    .stroke(style: StrokeStyle(
+                        lineWidth: lineWidth,
+                        dash: dashPattern,
+                        dashPhase: dashPhase))
+                    .foregroundColor(.blue)
+                    .onReceive(timer) { _ in
+                        timerCount = timerCount > dashPattern.reduce(0){ $0 + $1 } ? 0 : timerCount + 1
+                        dashPhase = timerCount
+                    }
+            }
+        }
+        .background(.black.opacity(0.5))
+    }
+    
+    private var RopePath: Path {
         Path { path in
-            path.move(to: CGPoint(x: 100, y: 100))
-            path.addQuadCurve(to: CGPoint(x: 300, y: 100),
+            path.move(to: CGPoint(x: 10, y: 100))
+            path.addQuadCurve(to: CGPoint(x: 400, y: 100),
                               control: CGPoint(x: 200, y: 200))
         }
-        .stroke(lineWidth: 6)
-        .foregroundStyle(
-            .linearGradient(
-                colors: [.pink, .blue, .pink],
-                startPoint: .leading,
-                endPoint: .trailing
-            )
-        )
-        .shadow(color: .black.opacity(1.0), radius: 8, x: 0, y: 15)
     }
+
 }
 
 struct RopeView_Previews: PreviewProvider {
     static var previews: some View {
-        RopeView()
+        RopeView(color: .blue, isGlowing: false)
     }
 }
