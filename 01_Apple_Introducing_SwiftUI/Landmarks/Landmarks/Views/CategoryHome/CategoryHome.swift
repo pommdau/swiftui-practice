bfbedf01d4abbb16f6f1ff61878056dc91414998
IKEH1024 <pondau1024@gmail.com>
2022-08-02 02:03:38 +0900

    wip

---
+++ 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/Views/CategoryHome/CategoryHome.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by HIROKI IKEUCHI on 2022/08/02.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
