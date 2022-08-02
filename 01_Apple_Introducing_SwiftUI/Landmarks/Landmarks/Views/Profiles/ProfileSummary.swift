49b37382503e486e7c014baa758b8174ebb18734
IKEH1024 <pondau1024@gmail.com>
2022-08-02 10:19:43 +0900

    wip

--- 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/Views/Profiles/ProfileSummary.swift
+++ 01_Apple_Introducing_SwiftUI/Landmarks/Landmarks/Views/Profiles/ProfileSummary.swift
@@ -9,17 +9,36 @@ import SwiftUI
 
 struct ProfileSummary: View {
     var profile: Profile
-
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 10) {
                 Text(profile.username)
                     .bold()
                     .font(.title)
-
+                
                 Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
                 Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))  // 色相回転
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)  // 0: 元の色, 1: 白黒
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
             }
         }
     }
