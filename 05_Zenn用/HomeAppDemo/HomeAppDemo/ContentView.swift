bb890b16cc69a15079042b2e1277850afa71fa22
IKEH1024 <pondau1024@gmail.com>
2022-08-05 10:18:47 +0900

    wip

--- 05_Zenn用/HomeAppDemo/HomeAppDemo/ContentView.swift
+++ 05_Zenn用/HomeAppDemo/HomeAppDemo/ContentView.swift
@@ -11,7 +11,8 @@ struct ContentView: View {
     var body: some View {
 //        LinearInterpolationView()
 //        QuadraticBezierView()
-        RopeViewWithoutAnimation()
+//        RopeViewWithoutAnimation()
+        SpringView()
     }
 }
 
--- 05_Zenn用/HomeAppDemo/HomeAppDemo/SpringAnimation/SpringView.swift
+++ 05_Zenn用/HomeAppDemo/HomeAppDemo/SpringAnimation/SpringView.swift
@@ -8,13 +8,94 @@
 import SwiftUI
 
 struct SpringView: View {
+    
+    @ObservedObject var physicsManager = PhysicsManager()
+    private let standardPoint = CGPoint(x: 200, y: 400)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.yellow
+                .contentShape(Rectangle())
+                .ignoresSafeArea()
+            
+            TimelineView(.periodic(from: Date(), by: physicsManager.frameRate)) { _ in
+                VStack {
+//                    Text("\(physicsManager.point.y)")
+                    ZStack {
+                        Circle()
+                            .position(CGPoint(x: standardPoint.x + physicsManager.point.x,
+                                              y: standardPoint.y))
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.red)
+                        Circle()
+                            .position(standardPoint)
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.blue)
+                    }
+                }
+            }
+            .position(x: 0, y: 0)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.green)
+        }
+        .onAppear {
+            physicsManager.startTimer()
+        }
+        .onTapGesture(coordinateSpace: .local) { location in
+            physicsManager.point.x = location.x
+            physicsManager.point.vx = 0
+        }
     }
 }
 
 struct SpringView_Previews: PreviewProvider {
     static var previews: some View {
         SpringView()
+    }
+}
+
+extension SpringView {
+    class PhysicsManager: ObservableObject {
+        
+        struct Point {
+            let mass: Double = 1  // 質量
+            var x: Double = 100
+            var vx: Double = 0
+        }
+
+        struct Spring {
+            let springLength: Double = 0
+            let k: Double = -200  // stiffness: 20
+            let d: Double = -2.0  // damping: 減衰振動
+        }
+            
+        private var timer: Timer? = nil
+        private let spring = Spring()
+        
+        var point = Point()
+        var frameRate: Double = 1 / 60
+        
+        func startTimer() {
+            timer = Timer.scheduledTimer(withTimeInterval: frameRate, repeats: true) { _ in
+                self.updateStatus()
+            }
+        }
+        
+        func stopTimer() {
+            timer?.invalidate()
+            timer = nil
+        }
+                               
+        private func updateStatus() {
+            let fSpringX = spring.k * (point.x - spring.springLength)  // フックの法則
+//            let fDampingX = spring.d * point.vx
+//            let ax = (fSpringX + fDampingX) / point.mass  // 加速度
+            
+            let ax = fSpringX / point.mass
+            
+            point.vx = point.vx + ax * frameRate
+            point.x = point.x + point.vx * frameRate
+        }
+        
     }
 }
