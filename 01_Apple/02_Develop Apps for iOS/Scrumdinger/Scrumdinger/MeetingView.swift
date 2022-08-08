9b1038146c098e705d9c175f6843e2f2e23336c5
IKEH1024 <pondau1024@gmail.com>
2022-08-08 13:59:45 +0900

    wip

--- 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/MeetingView.swift
+++ 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -14,16 +14,27 @@ struct MeetingView: View {
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
                 Spacer()
                 VStack(alignment: .leading) {
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
         }
-
+        .padding()
     }
 }
 
