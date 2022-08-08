840e130f57d820c74484f3257ae7a25287e6d57c
IKEH1024 <pondau1024@gmail.com>
2022-08-08 16:02:45 +0900

    wip

--- 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/Views/CardView.swift
+++ 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/Views/CardView.swift
@@ -35,7 +35,7 @@ struct CardView: View {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                    .padding(.trailing, 20)
+                    .labelStyle(.trailingIcon)
             }
             .font(.caption)
         }
