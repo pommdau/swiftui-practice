5293f922643364c6762cd1d0cf7fb79e6d59f255
IKEH1024 <pondau1024@gmail.com>
2022-08-15 23:50:53 +0900

    wip

--- 07_Kavsoft/2022/Grid Magnification Effect/TouchAnimation/TouchAnimation/Views/Home.swift
+++ 07_Kavsoft/2022/Grid Magnification Effect/TouchAnimation/TouchAnimation/Views/Home.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text("Home")
+        GeometryReader { proxy in
+            let size = proxy.size
+            
+            //  MARK: To Fit Into Whole View
+            // Calculating Item Count with the help of Height & Width
+            // In a Row We Have 10 Items
+            let width = (size.width / 10)
+            
+            // Multiplying Each Row Count
+            let itemCount = Int((size.height / width).rounded()) * 10
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 0), count: 10)) {
+                ForEach(0..<itemCount, id: \.self) { _ in
+                    GeometryReader { innerProxy in
+                        RoundedRectangle(cornerRadius: 4)
+                            .fill(.orange)
+                    }
+                    .padding(5)
+                    .frame(height: width)
+                }
+            }
+        }
+        .padding(15)
+        .preferredColorScheme(.dark)
     }
 }
 
