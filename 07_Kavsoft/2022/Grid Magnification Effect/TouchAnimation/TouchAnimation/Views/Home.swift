ac9ee8d4ea78c6cc254fc5863da1c58185a8e362
IKEH1024 <pondau1024@gmail.com>
2022-08-16 00:08:00 +0900

    wip

--- 07_Kavsoft/2022/Grid Magnification Effect/TouchAnimation/TouchAnimation/Views/Home.swift
+++ 07_Kavsoft/2022/Grid Magnification Effect/TouchAnimation/TouchAnimation/Views/Home.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct Home: View {
+    
+    // MARK: Gesture State
+    @GestureState var location: CGPoint = .zero
+        
     var body: some View {
+                
         GeometryReader { proxy in
             let size = proxy.size
             
@@ -19,11 +24,16 @@ struct Home: View {
             
             // Multiplying Each Row Count
             let itemCount = Int((size.height / width).rounded()) * 10
-            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 0), count: 10)) {
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 0), count: 10),
+                      spacing: 0) {
                 ForEach(0..<itemCount, id: \.self) { _ in
                     GeometryReader { innerProxy in
+                        let rect = innerProxy.frame(in: .named("GESTURE"))
+                        let scale = itemScale(rect: rect, size: size)
+                        
                         RoundedRectangle(cornerRadius: 4)
                             .fill(.orange)
+                            .scaleEffect(scale)
                     }
                     .padding(5)
                     .frame(height: width)
@@ -31,8 +41,33 @@ struct Home: View {
             }
         }
         .padding(15)
+        .gesture(
+            DragGesture(minimumDistance: 0)
+                .updating($location, body: { value, out, _ in
+                    out = value.location
+                })
+        )
+        .coordinateSpace(name: "GESTURE")
         .preferredColorScheme(.dark)
+        .animation(.easeInOut, value: location == .zero)
     }
+        
+    // MARK: Calculating scale for each item with the help of pythagorean theorem(ピタゴラスの定理)
+    
+    func itemScale(rect: CGRect, size: CGSize) -> CGFloat {
+        let a = location.x - rect.midX
+        let b = location.y - rect.midY
+        let root = sqrt(a * a + b * b)
+        let diagonalValue = sqrt(pow(size.width, 2) + pow(size.height, 2))
+                
+        // MARK: For more detail, devide diagonal value
+        let scale = root / (diagonalValue / 2)  // nomarization: 正規化？
+        let modifiedScale = location == .zero ? 1 : (1 - scale)
+        
+        // MARK: To avoid SwiftUI transform warning
+        return modifiedScale > 0 ? modifiedScale : 0.001
+    }
+        
 }
 
 struct Home_Previews: PreviewProvider {
