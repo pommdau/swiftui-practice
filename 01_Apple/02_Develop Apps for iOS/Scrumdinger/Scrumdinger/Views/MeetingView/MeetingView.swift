68abad9334220ab0209dedfcce89cd85b67bb26e
IKEH1024 <pondau1024@gmail.com>
2022-08-21 21:19:14 +0900

    wip until "Managing State and Life Cycle"

--- 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/Views/MeetingView/MeetingView.swift
+++ 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/Views/MeetingView/MeetingView.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
     
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
     
     var body: some View {
         ZStack {
@@ -32,6 +35,10 @@ struct MeetingView: View {
         .onAppear {
             scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes,
                              attendees: scrum.attendees)
+            scrumTimer.speakerChangedAction = {
+                player.seek(to: .zero)
+                player.play()
+            }
             scrumTimer.startScrum()
         }
         .onDisappear {
