ffc8400271275c90395a0191e764fd290044e31e
IKEH1024 <pondau1024@gmail.com>
2022-08-21 20:24:50 +0900

    wip

---
+++ 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/Views/MeetingView/MeetingView.swift
@@ -0,0 +1,60 @@
+//
+//  MeetingView.swift
+//  Scrumdinger
+//
+//  Created by HIROKI IKEUCHI on 2022/08/08.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    
+    @Binding var scrum: DailyScrum
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            
+            VStack {
+                ProgressView(value: 5, total: 15)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Seconds Elapsed")
+                            .font(.caption)
+                        Label("300", systemImage: "hourglass.bottomhalf.fill")
+                    }
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        Text("Seconds Remaining")
+                            .font(.caption)
+                        Label("600", systemImage: "hourglass.tophalf.fill")
+                    }
+                }
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel("Time remaining")
+                .accessibilityValue("10 minutes")
+                
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+    }
+}
