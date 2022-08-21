e08b0854b0ec5b3ca902f0f91cd7c954e024d3b6
IKEH1024 <pondau1024@gmail.com>
2022-08-21 20:53:42 +0900

    wip

--- 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/Views/MeetingView/MeetingHeaderView.swift
+++ 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/Views/MeetingView/MeetingHeaderView.swift
@@ -11,6 +11,7 @@ struct MeetingHeaderView: View {
     
     let secondsElapsed: Int
     let secondsRemaining: Int
+    let theme: Theme
     
     private var totalSeconds: Int {
         secondsElapsed + secondsRemaining
@@ -28,6 +29,8 @@ struct MeetingHeaderView: View {
     var body: some View {
         VStack {
             ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
+            
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
@@ -39,18 +42,20 @@ struct MeetingHeaderView: View {
                     Text("Seconds Remaining")
                         .font(.caption)
                     Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                        .labelStyle(.trailingIcon)
                 }
             }
             .accessibilityElement(children: .ignore)
             .accessibilityLabel("Time remaining")
             .accessibilityValue("\(minutesRemaining) minutes")
+            .padding([.top, .horizontal])
         }
     }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubblegum)
             .previewLayout(.sizeThatFits)  // Previewに便利
     }
 }
