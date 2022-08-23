5844c60af480864dc9514198596f75582c7eadc2
IKEH1024 <pondau1024@gmail.com>
2022-08-23 10:57:45 +0900

    wip

---
+++ 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,63 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by HIROKI IKEUCHI on 2022/08/23.
+//
+
+import Foundation
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+    }
+    
+    static func load(completion: @escaping (Result<[DailyScrum], Error>) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let fileURL = try fileURL()
+                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
+                    // 初回読み込み時で、ファイルがない場合
+                    DispatchQueue.main.async {
+                        completion(.success([]))
+                    }
+                    return
+                }
+                
+                let dailyScrums = try JSONDecoder().decode([DailyScrum].self, from: file.availableData)
+                DispatchQueue.main.async {
+                    completion(.success(dailyScrums))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
+    static func sace(scrums: [DailyScrum], completion: @escaping (Result<Int, Error>) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let data = try JSONEncoder().encode(scrums)
+                let outfile = try fileURL()
+                try data.write(to: outfile)
+                DispatchQueue.main.async {
+                    completion(.success(scrums.count))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+}
