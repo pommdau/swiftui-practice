916d90e7da22441e8682cd901feffd5f60feed34
IKEH1024 <pondau1024@gmail.com>
2022-08-23 12:00:56 +0900

    wip

--- 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
+++ 01_Apple/02_Develop Apps for iOS/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -16,21 +16,21 @@ struct ScrumdingerApp: App {
         WindowGroup {
             NavigationView {
                 ScrumsView(scrums: $store.scrums) {
-                    ScrumStore.save(scrums: store.scrums) { result in
-                        if case .failure(let error) = result {
-                            fatalError(error.localizedDescription)
+                    // Save Actions
+                    Task {
+                        do {
+                            try await ScrumStore.save(scrums: store.scrums)
+                        } catch {
+                            fatalError("Error saving scrums.")
                         }
                     }
                 }
             }
-            .onAppear {
-                ScrumStore.load { result in
-                    switch result {
-                    case .failure(let error):
-                        fatalError(error.localizedDescription)
-                    case .success(let scrums):
-                        store.scrums = scrums
-                    }
+            .task {
+                do {
+                    store.scrums = try await ScrumStore.load()
+                } catch {
+                    fatalError("Error loading scrums.")
                 }
             }
         }
