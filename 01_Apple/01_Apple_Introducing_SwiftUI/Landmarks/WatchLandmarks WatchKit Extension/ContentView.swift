09804165a5aa10b182c7752070e33894b2c73092
IKEH1024 <pondau1024@gmail.com>
2022-09-13 08:42:23 +0900

    wip

---
+++ 01_Apple/01_Apple_Introducing_SwiftUI/Landmarks/WatchLandmarks WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by HIROKI IKEUCHI on 2022/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 01_Apple/01_Apple_Introducing_SwiftUI/Landmarks/WatchLandmarks WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by HIROKI IKEUCHI on 2022/09/13.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
