1311195292d64584e2e8b151cef50c922aea1cd7
IKEH1024 <pondau1024@gmail.com>
2022-09-13 18:10:50 +0900

    wip

---
+++ 01_Apple/01_Apple_Introducing_SwiftUI/Landmarks/MacLandmarks/LandmarkDetail.swift
@@ -0,0 +1,69 @@
+//
+//  LandmarkDetail.swift
+//  MacLandmarks
+//
+//  Created by HIROKI IKEUCHI on 2022/09/13.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                
+                HStack {
+                    Text(landmark.park)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+                
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    
+    static let modelData = ModelData()
+    
+    static var previews: some View {
+        Group {
+            LandmarkDetail(landmark: modelData.landmarks[0])
+                .environmentObject(modelData)
+                .frame(width: 850, height: 700)
+        }
+    }
+}
