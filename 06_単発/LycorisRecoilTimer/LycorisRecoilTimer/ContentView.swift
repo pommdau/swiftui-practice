7e33d0e3678e102247c2f4f150c151d70773cd47
IKEH1024 <pondau1024@gmail.com>
2022-09-27 14:11:57 +0900

    create project

---
+++ 06_単発/LycorisRecoilTimer/LycorisRecoilTimer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LycorisRecoilTimer
+//
+//  Created by HIROKI IKEUCHI on 2022/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
