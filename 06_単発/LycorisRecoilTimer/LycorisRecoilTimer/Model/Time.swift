0f59f249eba7ac961e9fbb0ca8546b104b4da33d
IKEH1024 <pondau1024@gmail.com>
2022-09-29 10:00:38 +0900

    wip

--- 06_単発/LycorisRecoilTimer/LycorisRecoilTimer/Model/Time.swift
+++ 06_単発/LycorisRecoilTimer/LycorisRecoilTimer/Model/Time.swift
@@ -8,7 +8,26 @@
 import Foundation
 
 struct Time {
-    var hour: Int = 0
-    var minute: Int = 0
-    var second: Int = 0
+    var hour: Int
+    var minute: Int
+    var second: Int
+    
+    var totalSeconds: Double {
+        return Double(hour * 3600 + minute * 60 + second)
+    }
+    
+    init(hour: Int = 0, minute: Int = 0, second: Int = 0) {
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+    }
+    
+    init(seconds: Int) {
+        let hour = Int(seconds / 3600)
+        let minute = (seconds % 3600) / 60
+        let second = seconds % 60        
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+    }
 }
--- 06_単発/LycorisRecoilTimer/LycorisRecoilTimer/ViewModel/TimerViewModel.swift
+++ 06_単発/LycorisRecoilTimer/LycorisRecoilTimer/ViewModel/TimerViewModel.swift
@@ -8,35 +8,43 @@
 import Foundation
 
 final class TimerViewModel: ObservableObject {
-            
-    @Published var timerText: String = "xx:xx:xx"
+                
     @Published var isTimeOver: Bool = false
-    @Published var time = Time()    
+    @Published var time = Time()
     var timeBuffer = Time()
     
+    var timerText: String {
+        return String(format: "%02d:%02d:%02d", time.hour, time.minute, time.second)
+    }
+    
+    var isTimerValid: Bool {
+        if let timer = timer,
+           timer.isValid {
+            return true
+        } else {
+            return false
+        }
+    }
+        
     private var timer: Timer? = nil
-    private var startTime: TimeInterval? = Date.timeIntervalSinceReferenceDate + 5.0
+    private var startTime: TimeInterval = Date.timeIntervalSinceReferenceDate + 5.0
     
     func rightEyeClicked() {
         startTimer()
     }
     
     private func startTimer() {
+        
+//        guard isTimerValid else { return }
+                
+        startTime = Date.timeIntervalSinceReferenceDate + time.totalSeconds
         timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
-            guard let startTime = self.startTime else {
-                return
-            }
-            let remainTime = startTime - Date.timeIntervalSinceReferenceDate
-            
+            let remainTime = self.startTime - Date.timeIntervalSinceReferenceDate
             if remainTime <= 0 {
                 self.isTimeOver = true
-                self.timerText = "00:00:00"
                 self.stopTimer()
             } else {
-                let min = Int(remainTime / 60)
-                let sec = Int(remainTime) % 60
-                let msec = Int((remainTime - Double(sec)) * 100.0)
-                self.timerText = String(format: "%02d:%02d:%02d", min, sec, msec)
+                self.time = Time(seconds: Int(remainTime))
             }
         }
     }
