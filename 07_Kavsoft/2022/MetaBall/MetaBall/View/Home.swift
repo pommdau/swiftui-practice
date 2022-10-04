9d71f72422226d2d18a8748b347a4c6ec5634100
IKEH1024 <pondau1024@gmail.com>
2022-10-04 13:41:13 +0900

    wip 4:43

--- 07_Kavsoft/2022/MetaBall/MetaBall/View/Home.swift
+++ 07_Kavsoft/2022/MetaBall/MetaBall/View/Home.swift
@@ -22,41 +22,48 @@ struct Home: View {
     
     @ViewBuilder
     private func SingleMetaBall() -> some View {
-        Canvas { context, size in
-            context.addFilter(.alphaThreshold(min: 0.5, color: .white))
-            context.addFilter(.blur(radius: 30))
-            
-            // MARK: Drawing Layer
-            context.drawLayer { ctx in
-                // MARK: Placing symbols
-                for index in [1, 2] {
-                    if let resolvedView = context.resolveSymbol(id: index) {
-                        ctx.draw(resolvedView, at: CGPoint(x: size.width / 2,
-                                                           y: size.height / 2))
+        
+        Rectangle()
+            .fill(.linearGradient(colors: [Color("Gradient1"), Color("Gradient2")],
+                                  startPoint: .top,
+                                  endPoint: .bottom))
+            .mask {
+                Canvas { context, size in
+                    context.addFilter(.alphaThreshold(min: 0.5, color: .white))
+                    context.addFilter(.blur(radius: 10))
+                    
+                    // MARK: Drawing Layer
+                    context.drawLayer { ctx in
+                        // MARK: Placing symbols
+                        for index in [1, 2] {
+                            if let resolvedView = context.resolveSymbol(id: index) {
+                                ctx.draw(resolvedView, at: CGPoint(x: size.width / 2,
+                                                                   y: size.height / 2))
+                            }
+                        }
                     }
+                } symbols: {
+                    Ball()
+                        .tag(1)
+                    Ball(offset: dragOffset)
+                        .tag(2)
                 }
             }
-        } symbols: {
-            Ball()
-                .tag(1)
-            Ball(offset: dragOffset)
-                .tag(2)
-        }
-        .gesture(
-            DragGesture()
-                .onChanged({ value in
-                    dragOffset = value.translation
-                })
-                .onEnded({ _ in
-                    withAnimation(
-                        .interactiveSpring(response: 0.6,
-                                           dampingFraction: 0.7,
-                                           blendDuration: 0.7)
-                    ) {
-                        dragOffset = .zero
-                    }
-                })
-        )
+            .gesture(
+                DragGesture()
+                    .onChanged({ value in
+                        dragOffset = value.translation
+                    })
+                    .onEnded({ _ in
+                        withAnimation(
+                            .interactiveSpring(response: 0.6,
+                                               dampingFraction: 0.7,
+                                               blendDuration: 0.7)
+                        ) {
+                            dragOffset = .zero
+                        }
+                    })
+            )
     }
     
     @ViewBuilder
