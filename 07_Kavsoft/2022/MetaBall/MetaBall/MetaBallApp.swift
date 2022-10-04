d2ad95ddf9a9ec7b0c01d9a405d4b47ca2a75740
IKEH1024 <pondau1024@gmail.com>
2022-10-04 13:17:02 +0900

    create project

---
+++ 07_Kavsoft/2022/MetaBall/MetaBall/MetaBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaBallApp.swift
+//  MetaBall
+//
+//  Created by HIROKI IKEUCHI on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct MetaBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
