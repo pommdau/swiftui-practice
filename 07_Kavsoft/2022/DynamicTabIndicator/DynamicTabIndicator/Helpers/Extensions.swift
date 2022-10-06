f5da4511cc16704f676a2423a244de42a6ecdc09
IKEH1024 <pondau1024@gmail.com>
2022-10-06 17:31:47 +0900

    wip

--- 07_Kavsoft/2022/DynamicTabIndicator/DynamicTabIndicator/Helpers/Extensions.swift
+++ 07_Kavsoft/2022/DynamicTabIndicator/DynamicTabIndicator/Helpers/Extensions.swift
@@ -16,7 +16,7 @@ extension View {
         self
             .overlay {
                 GeometryReader { proxy in
-                    let minX = proxy.frame(in: .global).minX
+                    let minX = proxy.frame(in: .global).minX  // 現在のTabのleadingの位置
                     Color.clear
                         .preference(key: OffsetKey.self, value: minX)
                         .onPreferenceChange(OffsetKey.self) { value in
@@ -31,9 +31,9 @@ extension View {
 struct OffsetKey: PreferenceKey {
     static var defaultValue: CGFloat = 0
     
+    // reduce: 子Viewが入れた値から親Viewが受け取る値への変換処理を書きます。
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
+        value = nextValue()  // 新しい値で上書きするだけ
     }
     
 }
-
