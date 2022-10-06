99b51b5da4e433a50b19c5f22c89d8a877dbe294
IKEH1024 <pondau1024@gmail.com>
2022-10-06 15:55:34 +0900

    create initial project

---
+++ 07_Kavsoft/2022/DynamicTabIndicator/DynamicTabIndicator/DynamicTabIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTabIndicatorApp.swift
+//  DynamicTabIndicator
+//
+//  Created by HIROKI IKEUCHI on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTabIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
