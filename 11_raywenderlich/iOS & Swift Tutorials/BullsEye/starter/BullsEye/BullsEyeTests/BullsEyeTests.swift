bbc2fad742f1f6795b227a8690b01258baf5fdf5
IKEH1024 <pondau1024@gmail.com>
2022-10-23 20:36:23 +0900

    wip

---
+++ 11_raywenderlich/iOS & Swift Tutorials/BullsEye/starter/BullsEye/BullsEyeTests/BullsEyeTests.swift
@@ -0,0 +1,58 @@
+/// Copyright (c) 2022 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import XCTest
+@testable import BullsEye
+
+final class BullsEyeTests: XCTestCase {
+  
+  var sut: BullsEyeGame!  // system under test
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    
+    sut = BullsEyeGame()
+  }
+  
+  override func tearDownWithError() throws {
+    sut = nil
+    try super.tearDownWithError()
+  }
+      
+  func testScoreIsComputedWhenGuessIsHigherThanTarget() {
+    // given
+    let guess = sut.targetValue + 5  // 50 + 5 = 55
+    
+    // when
+    sut.check(guess: guess)  // 55(guess) - 50(targetValue)=5, 100-5=95点が期待値
+    
+    // then
+    XCTAssertEqual(sut.scoreRound, 95, "Score computed from guess is wrong")
+  }
+  
+}
