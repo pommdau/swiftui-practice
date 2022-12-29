2f4aef1bce2c3aa001980892e5705d1324aadcfe
IKEH1024 <pondau1024@gmail.com>
2022-12-29 16:21:52 +0900

    add: create project

---
+++ 06_単発/PageCurlDemo/PageCurlDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PageCurlDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
