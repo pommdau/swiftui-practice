8f130c1f55ff272ad9a54ed5741e0a441877e8f4
IKEH1024 <pondau1024@gmail.com>
2022-12-30 22:35:58 +0900

    wip

--- 99_Instance/DragDemo/DragDemo/BookView.swift
+++ 99_Instance/DragDemo/DragDemo/BookView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct BookView: View {
     
     @Binding var book: Book
+    @State private var center: CGPoint = .zero  // 初期位置
+    @State private var distanceFromCenter: CGPoint = .zero  // オブジェクトの中心とタッチ地点との距離
     
     var body: some View {
         HStack(spacing: 0) {
@@ -25,11 +27,25 @@ struct BookView: View {
                 .gesture(
                     DragGesture()
                         .onChanged({ value in
-//                            print(value.location)
+                            center = .init(x: center.x + 0.1, y: center.y + 0.1)
+                            distanceFromCenter = .init(x: distanceFromCenter.x + 0.1, y: distanceFromCenter.y + 0.1)
                             book.positon = CGPoint(x: book.positon.x + 0.1, y: book.positon.y + 0.1)
+                            
+//                            if distanceFromCenter == .zero {
+//                                distanceFromCenter = CGPoint(x: value.startLocation.x - center.x,
+//                                                             y: value.startLocation.y - center.y)
+//                            } else {
+//                                center = CGPoint(x: value.startLocation.x + value.translation.width - distanceFromCenter.x,
+//                                                 y: value.startLocation.y + value.translation.height - distanceFromCenter.y)
+//                                book.positon = center
+//                            }
+//                            book.positon = center
                         })
                         .onEnded({ value in
-                            book.positon = CGPoint(x: book.positon.x + 10, y: book.positon.y + 10)
+//                            book.positon = CGPoint(x: book.positon.x + 10, y: book.positon.y + 10)
+                            distanceFromCenter = .zero
+                            center = .zero
+                            print("onEnded")
                         })
                 )
             
