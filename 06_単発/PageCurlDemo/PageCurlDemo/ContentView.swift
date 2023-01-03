e15b167ff9e666f4141712daa5a2fbc71c9060b2
IKEH1024 <pondau1024@gmail.com>
2023-01-03 10:41:58 +0900

    wip

--- 06_単発/PageCurlDemo/PageCurlDemo/ContentView.swift
+++ 06_単発/PageCurlDemo/PageCurlDemo/ContentView.swift
@@ -6,8 +6,11 @@ struct ContentView: View {
     @State private var center: CGPoint = .init(x: 200, y: 200)  // 初期位置
     @State private var distanceFromCenter: CGPoint = .zero  // オブジェクトの中心とタッチ地点との距離
     
+    @State private var draggingFromDeckPosition: CGPoint = .zero
+    @State private var isDraggingFromDeck = false
+    
     var body: some View{
-        VStack {
+        ZStack {
             stickyCanvas()
             customToolbar()
         }
@@ -24,31 +27,61 @@ struct ContentView: View {
     
     @ViewBuilder
     private func customToolbar() -> some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(Color.accentColor, lineWidth: 4)
-                .frame(height: 80)
-                .background(Color.secondary.opacity(0.1).cornerRadius(10))
-            HStack {
-                StickyDeck()
-                    .frame(width: 100, height: 50)
-                    .padding(.leading, 20)
-                Spacer()
-                Button {
-                    addRandomSticky()
-                } label: {
-                    Image(systemName: "plus.circle")
+        GeometryReader { geometry in
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.accentColor, lineWidth: 4)
+                    .frame(height: 80)
+                    .background(Color.secondary.opacity(0.1).cornerRadius(10))
+                HStack {
+                    StickyDeck()
+                        .frame(width: 100, height: 50)
+                        .padding(.leading, 20)
+                        .gesture(
+                            DragGesture(coordinateSpace: .global)
+                                .onChanged({ value in
+                                    print(value.startLocation)
+                                    isDraggingFromDeck = true
+                                    if distanceFromCenter == .zero {
+                                        isDraggingFromDeck = true
+                                        distanceFromCenter = CGPoint(x: -50, y: 50)
+                                    } else {
+                                        draggingFromDeckPosition =
+                                        CGPoint(x: value.startLocation.x + value.translation.width - distanceFromCenter.x,
+                                                y: value.startLocation.y + value.translation.height - distanceFromCenter.y)
+                                    }
+                                })
+                                .onEnded({ value in
+                                    isDraggingFromDeck = false
+                                    draggingFromDeckPosition = .zero
+                                    distanceFromCenter = .zero
+                                    print(draggingFromDeckPosition)
+                                })
+                        )
+                    Spacer()
+                    Button {
+                        addRandomSticky()
+                    } label: {
+                        Image(systemName: "plus.circle")
+                    }
+                    .padding(.trailing, 20)
                 }
-                .padding(.trailing, 20)
             }
-            .frame(height: 80)
         }
+        .frame(height: 80)
         .padding()
     }
     
     @ViewBuilder
     private func stickyCanvas() -> some View {
         GeometryReader { geometry in
+            
+            if isDraggingFromDeck {
+                Rectangle()
+                    .frame(width: 100, height: 100)
+                    .position(draggingFromDeckPosition)
+            }
+            
             ForEach(stickyList.stickies.indices, id: \.self) { index in
                 ZStack {
                     Pages(currentPage: $stickyList.stickies[index].currentPageIndex,
@@ -67,7 +100,6 @@ struct ContentView: View {
                         .gesture(
                             DragGesture(coordinateSpace: .global)
                                 .onChanged({ value in
-                                    print(value)
                                     if distanceFromCenter == .zero {
                                         distanceFromCenter = CGPoint(x: value.startLocation.x - stickyList.stickies[index].positon.x,
                                                                      y: value.startLocation.y - stickyList.stickies[index].positon.y)
