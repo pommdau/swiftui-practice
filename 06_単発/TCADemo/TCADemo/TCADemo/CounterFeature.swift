60bfefb2206e1ef84c8f12cd476079ab3cf09b17
IKEH1024 <pondau1024@gmail.com>
2024-02-13 13:39:32 +0900

    wip

--- 06_単発/TCADemo/TCADemo/TCADemo/CounterFeature.swift
+++ 06_単発/TCADemo/TCADemo/TCADemo/CounterFeature.swift
@@ -45,6 +45,8 @@ struct CounterFeature {
         case timer
     }
     
+    @Dependency(\.continuousClock) var clock
+    
     var body: some ReducerOf<Self> {
         // state:  CounterFeature.State
         // action: CounterFeature.Action
@@ -85,8 +87,7 @@ struct CounterFeature {
                 state.isTimerRunning.toggle()
                 if state.isTimerRunning {
                     return .run { send in
-                        while true {
-                            try await Task.sleep(for: .seconds(1))
+                        for await _ in self.clock.timer(interval: .seconds(1)) {
                             await send(.timerTick)
                         }
                     }
--- 06_単発/TCADemo/TCADemo/TCADemoTests/CounterFeatureTests.swift
+++ 06_単発/TCADemo/TCADemo/TCADemoTests/CounterFeatureTests.swift
@@ -20,4 +20,26 @@ final class CounterFeatureTests: XCTestCase {
             $0.count = 0
         }
     }
+    
+    func testTimer() async {
+        let clock = TestClock()
+        
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        } withDependencies: {
+            $0.continuousClock = clock
+        }
+        
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = true
+        }
+        await clock.advance(by: .seconds(1))
+        await store.receive(\.timerTick) {
+            $0.count = 1
+        }
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = false
+        }
+    }
+    
 }
