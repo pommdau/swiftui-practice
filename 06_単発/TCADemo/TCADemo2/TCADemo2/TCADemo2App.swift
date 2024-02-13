aa937a1f3c9f65b2988a16861b70d9c805d1f7cd
IKEH1024 <pondau1024@gmail.com>
2024-02-13 14:35:01 +0900

    wip

--- 06_単発/TCADemo/TCADemo2/TCADemo2/TCADemo2App.swift
+++ 06_単発/TCADemo/TCADemo2/TCADemo2/TCADemo2App.swift
@@ -5,13 +5,20 @@
 //  Created by HIROKI IKEUCHI on 2024/02/13.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCADemo2App: App {
+    
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: TCADemo2App.store)
         }
     }
 }
